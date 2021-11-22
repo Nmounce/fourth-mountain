01bb466a706ebe069f3f7a88c84b8ade4cbcf049
Nikki Mounce <nikkimounce@gmail.com>
2021-11-22 14:57:26 -0800

    added home.js files

---
+++ src/components/home/type.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TypeWriter from "typewriter-effect";
+
+function Type() {
+    return (
+        <TypeWriter
+            options={{
+                strings: [
+                    "FULL STACK / WEB DEVELOPMENT / AND DESIGN"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+            }}
+        />
+    );
+}
+
+export default Type;
