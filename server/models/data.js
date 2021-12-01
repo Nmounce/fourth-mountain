46cecc9b00e5c5177a99619c50e9308ff16b350b
Nikki Mounce <nikkimounce@gmail.com>
2021-11-30 17:35:41 -0800

    removed server

--- server/models/data.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-
-const numOfLikes = schema({
-    likes: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model('likes', numOfLikes);
